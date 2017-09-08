dcac0950754ab50b45331f4847bf00e28e616371
Stanley Bak <stanleybak@gmail.com>
2017-09-08 16:41:06 -0400

    modded spring example

--- examples/spring/spring.py
+++ examples/spring/spring.py
@@ -28,9 +28,9 @@ def define_ha():
     # each mass will add 2 dimensions to the system
     #num_masses = 5
     #num_masses = 100
-    #num_masses = 500 # 1 thousand dims
+    num_masses = 500 # 1 thousand dims
     #num_masses =  2500 # 5 thousand dims
-    num_masses = 5000 # 10 thousand dims -> 800 MB
+    #num_masses = 5000 # 10 thousand dims -> 800 MB
     #num_masses = 50000 # 100 thousand dims -> memory error (80 GB mem needed)
     #num_masses = 500000 # one million dims (8 TB mem needed)
     a_matrix = make_a_matrix(num_masses)
