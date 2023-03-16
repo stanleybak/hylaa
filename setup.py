d2af034f4da897118cb727ea07507c15412aa013
Ethan Lew <elew@galois.com>
2023-03-16 14:01:43 -0700

    update setup.py with classifiers and long_description (to prep for publishing)

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@
 from setuptools import setup
 import versioneer
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="hylaa",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Hylaa",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/stanleybak/hylaa",
     author="Stanley Bak",
     author_email="bak2007-DONTSENDMEEMAIL@gmail.com",
@@ -25,5 +30,13 @@ setup(
         "termcolor",
         "swiglpk",
         "graphviz",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering"
     ],
 )
